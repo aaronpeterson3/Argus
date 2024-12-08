aa096cec1d6d15e41cc95ffa1bb409f7d67c3500
aaronpeterson3 <aaron@kronosautomation.com>
2024-12-07 17:58:59 -0700

    refactor: Update namespaces to Argus

---
+++ src/Argus.Abstractions/IUserGrain.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Orleans;
+
+namespace Argus.Abstractions
+{
+    public interface IUserGrain : IGrainWithStringKey
+    {
+        Task<bool> ValidateCredentialsAsync(string password);
+        Task<bool> ChangePasswordAsync(string currentPassword, string newPassword);
+        Task<UserState> GetStateAsync();
+        Task<bool> CreateAsync(string email, string password, UserProfile profile);
+    }
+
+    public record UserState
+    {
+        public string Email { get; init; }
+        public UserProfile Profile { get; init; }
+        public bool IsActive { get; init; }
+    }
+
+    public record UserProfile
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string DisplayName { get; init; }
+    }
+}
