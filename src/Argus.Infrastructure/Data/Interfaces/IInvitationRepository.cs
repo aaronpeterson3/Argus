5a7948e00b6d2eabd3e2d373ca16e94162a314aa
aaronpeterson3 <aaron@kronosautomation.com>
2024-12-09 14:28:00 -0700

    Update IInvitationRepository to use DTO

--- src/Argus.Infrastructure/Data/Interfaces/IInvitationRepository.cs
+++ src/Argus.Infrastructure/Data/Interfaces/IInvitationRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Argus.Infrastructure.Data.Interfaces
+using Argus.Infrastructure.Data.DTOs;
+
+namespace Argus.Infrastructure.Data.Interfaces
 {
     public interface IInvitationRepository
     {
+        Task CreateAsync(InvitationDto invitation);
+        Task<InvitationDto> GetPendingInvitationAsync(string email, string token);
+        Task UpdateStatusAsync(Guid id, string status);
     }
 }
