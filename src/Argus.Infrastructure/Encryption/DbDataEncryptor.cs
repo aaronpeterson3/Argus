586996077b1316f5742da4f6da289e9e1af79eb2
aaronpeterson3 <aaron@kronosautomation.com>
2024-12-09 07:30:18 -0700

    fix: Add explicit parameters to JsonSerializer calls to fix expression tree error

--- src/Argus.Infrastructure/Encryption/DbDataEncryptor.cs
+++ src/Argus.Infrastructure/Encryption/DbDataEncryptor.cs
@@ -1,25 +1,24 @@
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System.Text.Json;
 
-namespace Argus.Infrastructure.Encryption
-{
-    public class EncryptedStringConverter : ValueConverter<string, string>
-    {
-        public EncryptedStringConverter(IDataEncryption encryption)
-            : base(
-                v => encryption.Encrypt(v),
-                v => encryption.Decrypt(v))
-        {
-        }
-    }
+namespace Argus.Infrastructure.Encryption;
 
-    public class EncryptedJsonConverter<T> : ValueConverter<T, string>
+public class EncryptedStringConverter : ValueConverter<string, string>
+{
+    public EncryptedStringConverter(IDataEncryption encryption)
+        : base(
+            v => encryption.Encrypt(v),
+            v => encryption.Decrypt(v))
     {
-        public EncryptedJsonConverter(IDataEncryption encryption)
-            : base(
-                v => encryption.Encrypt(JsonSerializer.Serialize(v)),
-                v => JsonSerializer.Deserialize<T>(encryption.Decrypt(v)))
-        {
-        }
+    }
+}
+
+public class EncryptedJsonConverter<T> : ValueConverter<T, string>
+{
+    public EncryptedJsonConverter(IDataEncryption encryption)
+        : base(
+            v => encryption.Encrypt(JsonSerializer.Serialize(v, (JsonSerializerOptions)null)),
+            v => JsonSerializer.Deserialize<T>(encryption.Decrypt(v), (JsonSerializerOptions)null))
+    {
     }
 }
